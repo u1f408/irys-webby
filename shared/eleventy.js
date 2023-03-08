428c11699ef3a59d7b5b0d7a57289334280c57ed
Iris System <iris@iris.ac.nz>
2023-03-09 01:32:28 +1300

    shared, iryscc: expose current git rev

--- shared/eleventy.js
+++ shared/eleventy.js
@@ -1,10 +1,23 @@
+const path = require('node:path')
+const fs = require('node:fs')
+
 const shared = require('./index.js')
+
+function getGitRev() {
+	const rev = fs.readFileSync(path.resolve(shared.ROOT_DIR, '.git/HEAD')).toString().trim().split(/.*[: ]/).slice(-1)[0]
+    if (rev.indexOf('/') === -1) {
+        return rev
+    } else {
+        return fs.readFileSync(path.resolve(shared.ROOT_DIR, '.git/' + rev)).toString().trim()
+    }
+}
 
 module.exports = function(input) {
 	return function(eleventyConfig) {
 		eleventyConfig.addGlobalData('meow', {
 			rootDir: shared.ROOT_DIR,
 			assetBase: process.env.ASSET_BASE || '//assets.irys.cc',
+			gitRev: process.env.GIT_REVISION || getGitRev(),
 		})
 
 		return input(eleventyConfig)
--- src/iryscc/index.md
+++ src/iryscc/index.md
@@ -10,3 +10,9 @@ Meow!
 * [PKfronters](/fronters/) - a site (and embeddable widget) for displaying your system's fronters
 * [listy](/pk/listy/) - `pk;list` but in a browser
 * [grep](/pk/grep/) - "advanced" search for your system members
+
+## Site info
+
+This website is open source - see [github.com/u1f408/irys-webby](<https://github.com/u1f408/irys-webby>).
+
+You are looking at Git revision <code>{{ meow.gitRev }}</code>.
