dabccf29f4023de88f1efa6f68002df5518a5c86
Iris System <iris@iris.ac.nz>
2025-09-05 13:55:19 +1200

    iryscc: add analytics

--- shared/eleventy.js
+++ shared/eleventy.js
@@ -4,7 +4,7 @@ const fs = require('node:fs')
 const shared = require('./index.js')
 
 function getGitRev() {
-	const rev = fs.readFileSync(path.resolve(shared.ROOT_DIR, '.git/HEAD')).toString().trim().split(/.*[: ]/).slice(-1)[0]
+    const rev = fs.readFileSync(path.resolve(shared.ROOT_DIR, '.git/HEAD')).toString().trim().split(/.*[: ]/).slice(-1)[0]
     if (rev.indexOf('/') === -1) {
         return rev
     } else {
@@ -13,13 +13,14 @@ function getGitRev() {
 }
 
 module.exports = function(input) {
-	return function(eleventyConfig) {
-		eleventyConfig.addGlobalData('meow', {
-			rootDir: shared.ROOT_DIR,
-			assetBase: process.env.ASSET_BASE || '//assets.irys.cc',
-			gitRev: process.env.GIT_REVISION || getGitRev(),
-		})
+    return function(eleventyConfig) {
+        eleventyConfig.addGlobalData('meow', {
+            rootDir: shared.ROOT_DIR,
+            assetBase: process.env.ASSET_BASE || '//assets.irys.cc',
+            gitRev: process.env.GIT_REVISION || getGitRev(),
+            isDeploymentBuild: (process.env.IS_DEPLOYMENT_BUILD || '') === '1',
+        })
 
-		return input(eleventyConfig)
-	}
+        return input(eleventyConfig)
+    }
 }
